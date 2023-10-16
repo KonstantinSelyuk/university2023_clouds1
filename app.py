450708b29876ae45b5f7b8785eba4edbed7e663a
Kostiantyn Seliuk <konstantin.selyuk@yahoo.com>
2023-10-16 16:14:55 +0300

    Change main app code from example to real task (task5)
    Task5:
    Write simple WEB service app which will print how many days left to the nearest Saturday

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+def task5():
+    today = datetime.today()
+    days_to_saturday = (5 - today.weekday()) % 7
+    return f"There are {days_to_saturday} days left to the nearest Saturday."
